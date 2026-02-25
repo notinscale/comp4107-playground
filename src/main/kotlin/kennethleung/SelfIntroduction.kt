349be5d0d64850937f5af3a50a0b546fa907c433
Leung King Kai Kenneth <23236981@life.hkbu.edu.hk>
2026-02-25 17:58:30 +0800

    Updated Introduction

---
+++ src/main/kotlin/kennethleung/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.kennethleung
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is <LEUNG King Kai Kenneth>.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
